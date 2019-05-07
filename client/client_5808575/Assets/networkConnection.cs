fc25bc1455110464f413ca3b418dabfe3d59ead1
newlusifer <newlusiferkp@gmail.com>
2019-05-07 14:27:45 +0700

    connectServerAndClient

---
+++ client/client_5808575/Assets/networkConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class networkConnection : MonoBehaviour
+{
+    public static SocketIOComponent socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+
+        socket.On("hello",OnHello);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnHello(SocketIOEvent e)
+    {
+        Debug.Log("Hello");
+    }
+}
