c1213382c2de78161762de7284500d3f09f78941
newlusifer <newlusiferkp@gmail.com>
2019-05-07 14:39:14 +0700

    connectC&S_1

---
+++ client/client_5808575/Assets/scripts/answer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SocketIO;
+
+public class answer : MonoBehaviour
+{
+    public InputField inPut;
+    public int getInput = 0;
+    public Text textShow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        networkConnection.socket.On("startGuess",OnStartGuess);
+    }
+
+    public void onGetInput()
+    {
+        getInput = int.Parse(inPut.text);
+        textShow.text = getInput.ToString();
+        Debug.Log(getInput);
+    }
+
+    void OnStartGuess (SocketIOEvent e)
+    {
+        textShow.text = "StartGuess";
+    }
+}
